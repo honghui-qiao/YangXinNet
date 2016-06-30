defddd32abf0a1c90bf2174c39f429a60a44de1e
Michael Qiao <honghui.qiao@outlook.com>
2016-06-30 22:07:37 +0800

    Code check in

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+﻿var navCollapsed = true,
+    nav = $('#J_nav'),
+    bodyWidth = $('body').width();
+
+$(window).on('resize', function (e) {
+    bodyWidth = $('body').width();
+
+    if (bodyWidth <= 750) {
+        hideNav();
+    } else {
+        showNav();
+    }
+});
+
+function hideNav() {
+    nav.hide();
+    navCollapsed = true;
+}
+
+function showNav() {
+    nav.fadeIn('fast');
+    navCollapsed = false;
+}
+
+$('body').on('click', '#J_navTrigger', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (navCollapsed) {
+        showNav();
+    } else {
+        hideNav();
+    }
+});
+
+$('body').on('touchstart mousedown', function (e) {
+    if (bodyWidth <= 750) {
+        if ($(e.target).closest('#J_navTrigger,#J_nav').length > 0) {
+            return;
+        }
+
+        hideNav();
+    }
+});
